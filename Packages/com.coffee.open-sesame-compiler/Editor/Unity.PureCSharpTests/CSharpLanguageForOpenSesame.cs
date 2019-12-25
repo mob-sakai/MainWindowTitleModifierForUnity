981f541f8f7e3af2cbab6df066c389da3c80aaf4
mob-sakai <sakai@mobcast.jp>
2019-12-25 20:21:12 +0900

    ...

---
+++ Packages/com.coffee.open-sesame-compiler/Editor/Unity.PureCSharpTests/CSharpLanguageForOpenSesame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Scripting;
+using UnityEditor.Scripting.Compilers;
+using UnityEditor.Scripting.ScriptCompilation;
+using UnityEditorInternal;
+
+namespace Coffee.OpenSesameCompilers
+{
+    internal class CSharpLanguageForOpenSesame : CSharpLanguage
+    {
+#if UNITY_2019_3_OR_NEWER
+        public override ScriptCompilerBase CreateCompiler(ScriptAssembly scriptAssembly, EditorScriptCompilationOptions options, string tempOutputDirectory)
+#else
+		public override ScriptCompilerBase CreateCompiler(ScriptAssembly scriptAssembly, MonoIsland island, bool buildingForEditor, BuildTarget targetPlatform, bool runUpdater)
+#endif
+        {
+            // No asmdef -> default compiler
+            if (string.IsNullOrEmpty(scriptAssembly.OriginPath))
+#if UNITY_2019_3_OR_NEWER
+                return base.CreateCompiler(scriptAssembly, options, tempOutputDirectory);
+#else
+				return base.CreateCompiler(scriptAssembly, island, buildingForEditor, targetPlatform, runUpdater);
+#endif
+
+            // Do not use OpenSesameCompiler -> default compiler
+            var asmdefPath = Directory.GetFiles(scriptAssembly.OriginPath, "*.asmdef").Select(x => x.Replace(Environment.CurrentDirectory + Path.DirectorySeparatorChar, "")).FirstOrDefault();
+            var setting = OpenSesameSetting.CreateFromAsmdef(asmdefPath);
+
+            // No OpenSesameCompiler setting in meta.
+            if (setting == null || !setting.OpenSesameCompiler)
+#if UNITY_2019_3_OR_NEWER
+                return base.CreateCompiler(scriptAssembly, options, tempOutputDirectory);
+#else
+				return base.CreateCompiler(scriptAssembly, island, buildingForEditor, targetPlatform, runUpdater);
+#endif
+
+            // If publish is set, change output path.
+            var path = OpenSesameSetting.PublishOrigin;
+            if (path == scriptAssembly.OriginPath)
+            {
+                OpenSesameSetting.PublishOrigin = null;
+
+                //scriptAssembly.Filename = Path.GetFileName(setting.PublishFolder);
+                if (Directory.Exists(setting.PublishFolder))
+                    scriptAssembly.OutputDirectory = setting.PublishFolder;
+                else
+                {
+                    var asmdef = AssetDatabase.LoadAssetAtPath<AssemblyDefinitionAsset>(asmdefPath);
+                    UnityEngine.Debug.LogWarningFormat(asmdef, "Directory '{0}' is not found", setting.PublishFolder);
+                }
+            }
+
+            // Use OpenSesameCompiler.
+#if UNITY_2019_3_OR_NEWER
+            return new OpenSesameCompiler(scriptAssembly, options, tempOutputDirectory);
+#else
+			return new OpenSesameCompiler(scriptAssembly, island, runUpdater);
+#endif
+        }
+
+    }
+}
---
+++ Packages/com.coffee.open-sesame-compiler/Editor/Unity.PureCSharpTests/Compiler.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Scripting;
+using UnityEditor.Scripting.Compilers;
+using UnityEditor.Scripting.ScriptCompilation;
+using UnityEditor.Utils;
+using UnityEngine;
+
+namespace Coffee.OpenSesameCompilers
+{
+    internal class OpenSesameCompiler : MicrosoftCSharpCompiler
+    {
+#if UNITY_2019_3_OR_NEWER
+		public OpenSesameCompiler(ScriptAssembly scriptAssembly, EditorScriptCompilationOptions options, string tempOutputDirectory) : base(scriptAssembly, options, tempOutputDirectory)
+        {
+        }
+#else
+        ScriptAssembly scriptAssembly;
+
+        public OpenSesameCompiler(ScriptAssembly scriptAssembly, MonoIsland island, bool runUpdater) : base(island, runUpdater)
+        {
+            this.scriptAssembly = scriptAssembly;
+        }
+#endif
+
+        protected override Program StartCompiler()
+        {
+            // Kill previous process.
+            var p = base.StartCompiler();
+            p.Kill();
+
+#if UNITY_2019_3_OR_NEWER
+            var responsefile = assembly.GeneratedResponseFile;
+            var outputPath = assembly.FullPath;
+#else
+            // Get last responsefile.
+            var outopt = string.Format("/out:\"{0}\"", m_Island._output);
+            var responsefile = Directory.GetFiles("Temp", "UnityTempFile*")
+                    .OrderByDescending(f => File.GetLastWriteTime(f))
+                    .First(path => File.ReadAllLines(path).Any(line => line.Contains(outopt)));
+            var outputPath = scriptAssembly.FullPath;
+#endif
+
+            // Start compiling with dotnet app
+            const string compiler = "Packages/com.coffee.open-sesame-compiler/Compiler~";
+            var psi = new ProcessStartInfo()
+            {
+                Arguments = string.Format("run -p {0} -- -l {1}.log {1}", compiler, responsefile),
+                FileName = "dotnet",
+                CreateNoWindow = true
+            };
+
+            // On MacOS or Linux, PATH environmant is not correct.
+            if (Application.platform != RuntimePlatform.WindowsEditor)
+                psi.FileName = "/usr/local/share/dotnet/dotnet";
+
+            var program = new Program(psi);
+            program.Start((s, e) =>
+            // Exit callback
+            {
+                if (program.ExitCode == 0)
+                {
+                    // If dll published, reimport assembly.
+                    if (!Path.GetDirectoryName(outputPath).StartsWith("Library/ScriptAssemblies"))
+                    {
+                        EditorApplication.delayCall += () =>
+                            AssetDatabase.ImportAsset(outputPath);
+                    }
+                    return;
+                }
+
+                // Show error logs.
+                foreach (var l in File.ReadAllLines(responsefile + ".log"))
+                    UnityEngine.Debug.LogError(l);
+            });
+
+            return program;
+        }
+    }
+}
