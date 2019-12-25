16daa6badcebebc9b15214c7a7b5671b7b6e95f0
mob-sakai <sakai@mobcast.jp>
2019-12-25 19:50:07 +0900

    for OpenSesameCompiler demo

--- Assets/Editor/Solution3.IgnoresAccessChecksToAttribute/Solution3.IgnoresAccessChecksToAttribute.cs
+++ Assets/Editor/Solution3.IgnoresAccessChecksToAttribute/Solution3.IgnoresAccessChecksToAttribute.cs
@@ -3,20 +3,26 @@ using UnityEditor;
 
 namespace MainWindowTitleModifier
 {
-    // To compile this assembly:
-    //   1. Select MainWindowTitleModifier.asmdef in project view
-    //   2. Right click and select 'Internal Accessible > Compile'
-    //   3. MainWindowTitleModifier.dll will be regenerated and reloaded.
-    //   4. Enjoy!
-    public class Solution3_IgnoresAccessChecksToAttribute
-    {
-        [MenuItem("MainWindowTitleModifier/Solution3_IgnoresAccessChecksToAttribute", priority = 3)]
-        static void Update()
-        {
-            Action<ApplicationTitleDescriptor> cb = x => x.title = "Solution3_IgnoresAccessChecksToAttribute";
-            EditorApplication.updateMainWindowTitle += cb;
-            EditorApplication.UpdateMainWindowTitle();
-            EditorApplication.updateMainWindowTitle -= cb;
-        }
-    }
+	// To compile this assembly:
+	//   1. Select MainWindowTitleModifier.asmdef in project view
+	//   2. Right click and select 'Internal Accessible > Compile'
+	//   3. MainWindowTitleModifier.dll will be regenerated and reloaded.
+	//   4. Enjoy!
+	public class Solution3_IgnoresAccessChecksToAttribute
+	{
+		[MenuItem("MainWindowTitleModifier/Solution3_IgnoresAccessChecksToAttribute", priority = 3)]
+		static void Update()
+		{
+			Action<ApplicationTitleDescriptor> cb = x => x.title = "Solution3_IgnoresAccessChecksToAttribute";
+			EditorApplication.updateMainWindowTitle += cb;
+			EditorApplication.UpdateMainWindowTitle();
+			EditorApplication.updateMainWindowTitle -= cb;
+		}
+
+		[MenuItem("Switch/Skin")]
+		static void Internal_SwitchSkin()
+		{
+			EditorApplication.Internal_SwitchSkin();
+		}
+	}
 }
